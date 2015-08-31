8ee8a9fe23f43e8d89b4062e6c99c14fb0b35304
Mark Robson <roppa_uk@hotmail.com>
2015-08-31 15:59:16 -0700

    Updated gitignore to ignore cloudinary. Added supertest to test node. Updated package and Grunt file

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,13 @@
-var express  = require('express'),
-    morgan   = require('morgan');
-
-// set the port
-var port = process.env.PORT || 8080;
-
+var express = require('express');
+var morgan = require('morgan');
 var app = express();
+
+//use correct config file for development, production
+app.config = require(__dirname + '/config/' + (process.env.NODE_ENV || 'development') + '/config');
+
+//cloudinary api
+app.cloudinary = require('cloudinary');
+app.cloudinary.config(require(__dirname + '/config/cloudinary'));
 
 // configure our server with all the middleware and and routing
 require('./config/middleware.js')(app, express);
@@ -14,9 +17,9 @@ require('./config/middleware.js')(app, express);
 //========================
 
 // startup our app
-app.listen(port);
+app.listen(app.config.port);
 
-console.log('Server currently chillin on port ' + port);
+console.log('Server currently chillin on port ' + app.config.port);
 
 // export for testing and flexibility
 module.exports = app;
