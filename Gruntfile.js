5e4ad30cca80f1285781084952377102fb1f3f6a
jimtom2713 <jwilliamson528@gmail.com>
2015-08-31 11:30:13 -0700

    added jsdoc to task runner and fixed .grunt error

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    // pkg: grunt.file.readJSON("package.json"),
     watch: {
-/*      grunt: {
+      /*grunt: {
         files: ["Gruntfile.js", "package.json"],
         tasks: "default"
       },*/
       javascript: {
-        files: ["Gruntfile.js", "src/client/**/*.js", "src/server/*.js", "specs/**/*Spec.js"],
+        files: [/*"Gruntfile.js",*/ "src/client/**/*.js", "src/server/*.js", "specs/**/*Spec.js"],
         tasks: "test"
       }
     },
@@ -29,17 +29,22 @@ module.exports = function(grunt) {
         "Gruntfile.js",
         "src/**/*.js",
         "spec/**/*.js"
-      ],
-      options: {
-        // jshintrc: ".jshintrc"
-      }
+      ]
     },
-    shell: {
-      options: {
-        stderr: false
-      },
-      target: {
-        command: 'mkdir .grunt'
+    // shell: {
+    //   options: {
+    //     stderr: false
+    //   },
+    //   target: {
+    //     command: 'mkdir .grunt'
+    //   }
+    // },
+    jsdoc: {
+      dist: {
+        src: ["Gruntfile.js", "src/client/**/*.js", "src/server/*.js", "specs/**/*Spec.js"],
+        options: {
+          destination: 'doc'
+        }
       }
     }
   });
@@ -48,53 +53,11 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-jasmine");
   grunt.loadNpmTasks("grunt-mocha-cli");
   grunt.loadNpmTasks("grunt-shell");
+  grunt.loadNpmTasks("grunt-jsdoc");
 
 
-  grunt.registerTask("test", ["jshint","shell", "mochacli", "jasmine"]);
+  grunt.registerTask("test", [/*"shell", */"jshint", "mochacli", "jasmine"]);
   grunt.registerTask("default", ["test"]);
+  grunt.registerTask("document", ["jsdoc"]);
+  grunt.registerTask("build", []);
 };
-
-
-
-
-// module.exports = function(grunt) {
-
-//   grunt.initConfig({
-//     jshint: {
-//       files: ['Gruntfile.js', 'server/**/*.js', 'test/**/*.js', 'client/**/*.js'],
-//       options: {
-//         globals: {
-//           jQuery: true
-//         }
-//       }
-//     },
-//     jsdoc: {
-//       dist: {
-//         src: ['server/**/*.js', 'test/**/*.js', 'client/**/*.js'],
-//         options: {
-//           destination: 'doc'
-//         }
-//       }
-//     },
-//     watch: {
-//       files: ['<%= jshint.files %>'],
-//       tasks: ['jshint']
-//     }
-//   });
-
-//   grunt.loadNpmTasks('grunt-contrib-jshint');
-//   grunt.loadNpmTasks('grunt-contrib-watch');
-//   grunt.loadNpmTasks('grunt-mocha');
-//   grunt.loadNpmTasks('grunt-contrib-uglify');
-//   grunt.loadNpmTasks('grunt-contrib-concat');
-//   grunt.loadNpmTasks('grunt-jsdoc');
-//   grunt.loadNpmTasks("grunt-contrib-jasmine");
-//   //eventually minification
-
-//   grunt.registerTask('default', ['jshint', 'watch']);
-//   grunt.registerTask('docs', ['jsdoc']);
-//   //uglify
-//   //minify
-//   //concat
-//   //use nodemon to start node/express application
-// };
