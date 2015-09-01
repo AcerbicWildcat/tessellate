82885d5d4dd70773368cb2550515aa61f8af2879
Robby Hays <rhaysvm42@gmail.com>
2015-09-01 12:13:35 -0700

    working on auth routes

--- src/server/config/auth-routes.js
+++ src/server/config/auth-routes.js
@@ -1,3 +1,31 @@
 module.exports = function (app, passport) {
+  // Landing Page Route
+  app.get('/', isLoggedIn, function (req, res){
+    res.render('/../../client/html/enter.html');
+  });
 
+  app.get('/login', function (req, res){
+    res.render('/../../client/html/login.html');
+  });
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'),
+    function (req, res){
+      // Request redirected to facebook so this function does not get called
+    });
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {failureRedirect: '/login'}),
+    function (req, res){
+      res.redirect('/');
+    });
+
+  app.get('/logout', function (req, res){
+    req.logout();
+    res.redirect('/login');
+  });
+
+  function isLoggedIn (req, res, next){
+    if (req.isAuth)
+  };
 };
