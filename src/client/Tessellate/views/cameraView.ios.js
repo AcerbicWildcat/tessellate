003b8a44cc0eff402ffbe02676285b031505f47c
Jonathan Schapiro <jschapir@gmail.com>
2015-08-31 19:45:57 -0700

    base camera functions

--- src/client/Tessellate/views/cameraView.ios.js
+++ src/client/Tessellate/views/cameraView.ios.js
@@ -1,58 +1,79 @@
-'use strict';
- 
 var React = require('react-native');
-
 var {
+  AppRegistry,
   StyleSheet,
-  TouchableHighlight,
-  View,
   Text,
-  Component,
-  TextInput,
-  
+  View,
+  TouchableHighlight
 } = React;
+var Camera = require('react-native-camera');
 
-var styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: 'black',
-	},
-	description: {
-    fontSize: 30,
-    textAlign: 'center',
-    color: '#FFFFFF'
+var CameraView = React.createClass({
+  getInitialState() {
+    return {
+      cameraType: Camera.constants.Type.back
+    }
   },
 
+  render() {
+    return (
+      <Camera
+        ref="cam"
+        style={styles.container}
+        type={this.state.cameraType}
+      >
+        <TouchableHighlight style={styles.button}
+        onPress={this._takePicture}>
+          <Text style={styles.buttonText}>Take Picture</Text>
+        </TouchableHighlight>
+
+      </Camera>
+    );
+  },
+
+  //switch camera view
+  _switchCamera() {
+    var state = this.state;
+    state.cameraType = state.cameraType === Camera.constants.Type.back
+      ? Camera.constants.Type.front : Camera.constants.Type.back;
+    this.setState(state);
+  },
+
+  //take picture
+  _takePicture() {
+    this.refs.cam.capture(function(err, data) {
+      console.log(err, data);
+    });
+  }
+
+
+  /*<TouchableHighlight onPress={this._switchCamera}>
+          <Text>The old switcheroo</Text>
+        </TouchableHighlight>*/
 });
 
 
-//Present Camera
-class CameraView extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-     
-    }
-  }
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  button: {
+        height: 36,
+        width:160,
+        backgroundColor: 'grey',
+        borderRadius: 8,
+        justifyContent: 'center',
+        marginTop: 15
+    },
+   buttonText: {
+        fontSize: 12,
+        color: 'white',
+        alignSelf: 'center'
+    },
 
-  render() {
-    //camera - https://github.com/lwansbrough/react-native-camera
-    return (
-
-      <View style={styles.container}>
-
-        <Text style={styles.description}>
-          Camera View
-        </Text>
-        
-      </View>
-      
-    );
-  }
-
-  
-}
+});
 
 module.exports = CameraView;
