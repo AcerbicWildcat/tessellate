17c554e31ee897001891848db86dcf2155576e74
Jonathan Schapiro <jschapir@gmail.com>
2015-08-31 19:57:12 -0700

    centered take picture button

--- src/client/Tessellate/views/cameraView.ios.js
+++ src/client/Tessellate/views/cameraView.ios.js
@@ -56,17 +56,21 @@ var CameraView = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
+    position:'relative',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'transparent',
   },
   button: {
+        position:'absolute',
+        bottom:50,
+        left:100,
         height: 36,
         width:160,
         backgroundColor: 'grey',
         borderRadius: 8,
-        justifyContent: 'center',
-        marginTop: 15
+
+       
     },
    buttonText: {
         fontSize: 12,
