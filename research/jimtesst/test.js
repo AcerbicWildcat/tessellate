4bc0308fea950c068254b5ee275f3b8cc2ab32e2
macklevine <mackplevine@gmail.com>
2015-09-01 20:08:21 -0700

    left some notes for future work

--- research/jimtesst/test.js
+++ research/jimtesst/test.js
@@ -136,25 +136,41 @@ var addToSVG = function(img, rgb) {
 
 function addToSVG (img, rgb) {
 
+  // { "_id" : ObjectId("55e65ccc3b1c5de4381f9f68"), "data" : { "0" : { "original" : true, "rgb" : { "b" : 216, "g" : 216, "r" : 216 }, "coords" : [ 0, 0 ] }, "1" : { "original" : true, "rgb" : { "b" : 253, "g" : 253, "r" : 253 }, "coords" : [ 1, 0 ] }, "2" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 2, 0 ] }, "3" : { "original" : true, "rgb" : { "b" : 62, "g" : 140, "r" : 62 }, "coords" : [ 3, 0 ] }, "4" : { "original" : true, "rgb" : { "b" : 62, "g" : 138, "r" : 62 }, "coords" : [ 4, 0 ] }, "5" : { "original" : true, "rgb" : { "b" : 79, "g" : 79, "r" : 79 }, "coords" : [ 0, 1 ] }, "6" : { "original" : true, "rgb" : { "b" : 88, "g" : 88, "r" : 88 }, "coords" : [ 1, 1 ] }, "7" : { "original" : true, "rgb" : { "b" : 178, "g" : 178, "r" : 178 }, "coords" : [ 2, 1 ] }, "8" : { "original" : true, "rgb" : { "b" : 98, "g" : 165, "r" : 98 }, "coords" : [ 3, 1 ] }, "9" : { "original" : true, "rgb" : { "b" : 196, "g" : 226, "r" : 196 }, "coords" : [ 4, 1 ] }, "10" : { "original" : true, "rgb" : { "b" : 162, "g" : 162, "r" : 162 }, "coords" : [ 0, 2 ] }, "11" : { "original" : true, "rgb" : { "b" : 183, "g" : 183, "r" : 183 }, "coords" : [ 1, 2 ] }, "12" : { "original" : true, "rgb" : { "b" : 220, "g" : 220, "r" : 220 }, "coords" : [ 2, 2 ] }, "13" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 3, 2 ] }, "14" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 4, 2 ] }, "15" : { "original" : true, "rgb" : { "b" : 154, "g" : 77, "r" : 91 }, "coords" : [ 0, 3 ] }, "16" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 1, 3 ] }, "17" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 2, 3 ] }, "18" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 3, 3 ] }, "19" : { "original" : true, "rgb" : { "b" : 192, "g" : 192, "r" : 255 }, "coords" : [ 4, 3 ] }, "20" : { "original" : true, "rgb" : { "b" : 139, "g" : 62, "r" : 67 }, "coords" : [ 0, 4 ] }, "21" : { "original" : true, "rgb" : { "b" : 221, "g" : 194, "r" : 197 }, "coords" : [ 1, 4 ] }, "22" : { "original" : true, "rgb" : { "b" : 255, "g" : 255, "r" : 255 }, "coords" : [ 2, 4 ] }, "23" : { "original" : true, "rgb" : { "b" : 200, "g" : 200, "r" : 255 }, "coords" : [ 3, 4 ] }, "24" : { "original" : true, "rgb" : { "b" : 1, "g" : 1, "r" : 255 }, "coords" : [ 4, 4 ] } }, "height" : 5, "width" : 5, "__v" : 0, "_parentEvent" : ObjectId("55e65cca3b1c5de4381f9f67") }
+
   var minimum;
-  var placeHolder;
+  var whatChunk;
+
+  //_storage will ultimately be replaced with map.data
 
   for(var key in _storage){
     var mainRGB = _storage[key].rgb;
     var RGBDistance = Math.sqrt(Math.pow(mainRGB.r - rgb.r, 2) + Math.pow(mainRGB.g - rgb.g, 2) + Math.pow(mainRGB.b - rgb.b, 2));
+    //the difference between the average RGB value of the small image and the average RGB value of the large image.
     if(minimum === undefined){
       minimum = RGBDistance;
-      placeHolder = _storage[key];
+      whatChunk = _storage[key];
     }
     if(RGBDistance < minimum && _storage[key].minValue === undefined){
       minimum = RGBDistance;
-      placeHolder = _storage[key];
+      whatChunk = _storage[key];
     }
   }
   
-  placeHolder.minValue = minimum;
-  placeHolder.original = false;
 
+  if (whatChunk.original === true){
+    whatChunk.minValue = minimum;
+    whatChunk.original = false;
+  } else {
+    //logic to handle collisions goes here.
+    //TODO: give the schema for the map object properties: minValue and imgPath
+    //TODO: on page load for the SVG view, iterate through the entire object
+    //and call renderImage for each image found.
+  }
+  renderImage(placeHolder.coords[0], placeHolder.coords[1]);
+}
+
+var renderImage = function(xCoord, yCoord){
   var svgImg = document.createElementNS('http://www.w3.org/2000/svg','image');
   svgImg.setAttributeNS(null,'height','10');
   svgImg.setAttributeNS(null,'width','10');
@@ -168,5 +184,4 @@ function addToSVG (img, rgb) {
   svgLink.appendChild(svgImg);
 
   document.getElementsByClassName('svg-pan-zoom_viewport')[0].appendChild(svgLink);
-
 }
