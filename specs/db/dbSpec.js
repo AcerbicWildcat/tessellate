698de1cad177aec9dea67f2fecc91967c01ca94d
macklevine <mackplevine@gmail.com>
2015-09-02 20:22:14 -0700

    wrote a test for the database and made one small tweak to the map saving function.

--- specs/db/dbSpec.js
+++ specs/db/dbSpec.js
@@ -1,89 +1,52 @@
-// var mongodb = require('mongodb');
+//mongo must be running and the server must be running for these
+//tests to pass. How do I guarantee this?
+
+
 var mongoose = require('mongoose');
-var request = require("request"); // You might need to npm install the request module!
+var request = require("request"); // Might want to swap out with supertest
 var expect = require('expect.js');
+// var db = require('./db.js')
 
 describe("Tessellate database", function() {
-  var dbConnection;
 
   beforeEach(function(done) {
     mongoose.connect('mongodb://localhost/3000');
-  });
-
-  afterEach(function() {
-    //wipe all the collections out of the database here.
-    mongoose.connection.db.dropDatabase();
-    mongoose.connection.close();
-  });
-
-  it("Should analyze an image and save a coordinate map to the database", function(done){
-    request({
-      
+    mongoose.connection.once('open', function(){
+      done(); //guarantees that the connection is open before tests proceed.
     })
-  })
-  // it("Should insert posted messages to the DB", function(done) {
-  //   // Post the user to the chat server.
-  //   request({ method: "POST",
-  //             uri: "http://localhost:3000/",
-  //             json: { username: "Valjean" }
-  //   }, function () {
-  //     // Post a message to the node chat server:
-  //     request({ method: "POST",
-  //             uri: "http://127.0.0.1:3000/classes/messages",
-  //             json: {
-  //               username: "Valjean",
-  //               message: "In mercy's name, three days is all I need.",
-  //               roomname: "Hello"
-  //             }
-  //     }, function () {
-  //       // Now if we look in the database, we should find the
-  //       // posted message there.
+  });
 
-  //       // TODO: You might have to change this test to get all the data from
-  //       // your message table, since this is schema-dependent.
-  //       var queryString = "SELECT * FROM messages";
-  //       var queryArgs = [];
+  afterEach(function(done) {
+    //wipe all the collections out of the database here.
+    mongoose.connection.db.dropDatabase(function(err){
+      mongoose.connection.close(function(){
+        done();
+      });
+    });
+  });
 
-  //       dbConnection.query(queryString, queryArgs, function(err, results) {
-  //         // Should have one result:
-  //         expect(results.length).to.equal(1);
+  xit("Should be able to save a user to the database", function(done){
 
-  //         // TODO: If you don't have a column named text, change this test.
-  //         expect(results[0].text).to.equal("In mercy's name, three days is all I need.");
+  });
 
-  //         done();
-  //       });
-  //     });
-  //   });
-  // });
+  xit("Should analyze an image and save a coordinate map to the database", function(done){
+    request({ method: "POST",
+              uri: "http://localhost:3000/", //need to add whatever route we use...
+              json: {
+                name: "mack",
+                tag: "#mackevent",
+                path: "hackreactor.jpg",
+                username: "mack"
+              }
+    }, function(){
+      mongoose.connection.db.collection("maps", function(err, collection){
 
-  // it("Should output all messages from the DB", function(done) {
-  //   var queryString = "INSERT INTO Users(username, createdAt, updatedAt) VALUES (?, ?, ?)";
-  //   var queryArgs = ["fred", '2014-11-22 08:52:22', '2014-11-22 08:52:22'];
-  //   // TODO - The exact query string and query args to use
-  //   // here depend on the schema you design, so I'll leave
-  //   // them up to you. */
-
-  //   dbConnection.query(queryString, queryArgs, function(err, results) {
-  //     // Let's insert a message into the db
-  //     var queryString = "INSERT INTO Messages(text, userid, roomname, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?)";
-  //     var queryArgs = ["Men like you can never change!", results.insertId, "main", '2014-11-22 08:52:22', '2014-11-22 08:52:22'];
-  //     // TODO - The exact query string and query args to use
-  //     // here depend on the schema you design, so I'll leave
-  //     // them up to you. */
-
-  //     dbConnection.query(queryString, queryArgs, function(err) {
-  //       if (err) { throw err; }
-
-  //       // Now query the Node chat server and see if it returns
-  //       // the message we just inserted:
-  //       request("http://127.0.0.1:3000/classes/messages", function(error, response, body) {
-  //         var messageLog = JSON.parse(body);
-  //         expect(messageLog[0].text).to.equal("Men like you can never change!");
-  //         expect(messageLog[0].roomname).to.equal("main");
-  //         done();
-  //       });
-  //     });
-  //   });
-  // });
+        collection.find({height: 250}).toArray(function(err, results){
+          expect(results[0].width).to.equal(850); //check height and width...
+          done();
+        });
+        //now do something to verify that the map showed up in there.
+      });
+    });
+  });
 });
