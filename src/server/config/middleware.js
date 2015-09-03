1e9c332dfd701a94fb1807e4e0324bb0d3d09af9
Robby Hays <rhaysvm42@gmail.com>
2015-09-02 17:33:35 -0700

    closes AcerbicWildcat/tessellate#42

--- src/server/config/middleware.js
+++ src/server/config/middleware.js
@@ -17,7 +17,7 @@ module.exports = function (app, express) {
   app.use(bodyParser.json());
 
   // set up sessions and initialize passport
-  // app.use(session({secret: 'tIndEr4KatzsNdDucks'}));
+  app.use(session({secret: 'tIndEr4KatzsNdDucks'}));
   app.use(passport.initialize());
   app.use(passport.session());
   // files in /client/public/ will be served as static assets
