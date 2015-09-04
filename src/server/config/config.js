c559ef7dd9294ac33125e781ffbfa29d899c7bf2
Mark Robson <roppa_uk@hotmail.com>
2015-09-03 17:17:35 -0700

    Refactored to use environmental variables.

---
+++ src/server/config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  port : process.env.PORT,
+  cloudinary : {
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME, 
+     api_key: process.env.CLOUDINARY_API_KEY,
+     api_secret: process.env.CLOUDINARY_API_SECRET
+  },
+  passport: {
+    facebook: {
+      clientID: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+      enableProof: process.env.FACEBOOK_ENABLE_PROOF
+    }
+  },
+  db: {
+    host: process.env.DB_HOST
+  },
+  sessionSecret: process.env.SESSION_SECRET
+};
