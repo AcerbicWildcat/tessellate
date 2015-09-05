1b9dd1d1adaaa5300f38101ccb8a7722816798d4
jimtom2713 <jwilliamson528@gmail.com>
2015-09-04 19:51:52 -0700

    file uploading template

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -32,72 +32,15 @@ var tess = angular.module("tessell", [
     // $httpProvider.interceptors.push('ResInterceptor');
   })
 
-  .config(['flowFactoryProvider', function (flowFactoryProvider) {
-    flowFactoryProvider.defaults = {
-      // target: 'upload.php',
-      permanentErrors: [404, 500, 501],
-      maxChunkRetries: 1,
-      chunkRetryInterval: 5000,
-      simultaneousUploads: 4,
-      singleFile: true
-    };
-    flowFactoryProvider.on('fileAdded', function (event) {
-      console.log('fileAdded', arguments);
-    });
-    // Can be used with different implementations of Flow.js
-    // flowFactoryProvider.factory = fustyFlowFactory;
-}]);
-
-tess.controller("tessellCtrl", function ($scope, $location){
-  $scope.testing = false;
-  $scope.eventTag = "";
-  $scope.createEvent = function(){
-    Upload.upload({
-      url: '/event',
-      file: $scope.event.file
-    });
-  };
-  $scope.go = function (event){
-    if($scope.eventTag === "" && event.keyCode === 13){
-      $scope.testing = true;
-    }
-    else if(event.keyCode === 13){
-      // console.log($scope.eventTag);
-      $scope.eventTag = "";
-      $scope.testing = false;
-    }
-    // $location.path( path );
-  };
-});
-
-tess.controller('DatepickerDemoCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
-
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
-
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
-
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
-
-  $scope.open = function($event) {
-    $scope.status.opened = true;
-  };
-
   tess.controller('tessellCtrl', ['$scope', "eventFactory", "$location",  function ($scope, eventFactory, $location){
+    // $scope.eventExisits = true;
     $scope.eventTag = "";
     $scope.checkForExistingEvent = function(){
       eventFactory.checkForExistingEvent($scope.eventTag);
+      // eventFactory.setEventTag($scope.eventTag);
+      // $scope.eventTag = "";
+      // $scope.eventExisits = false;
+      // $location.path('/dashboard');
     };
     $scope.dropzoneConfig = {
       'options': {
@@ -109,7 +52,7 @@ tess.controller('DatepickerDemoCtrl', function ($scope) {
       'eventHandlers': {
         'sending': function (file, xhr, formData) {
           // console.log(formData, file, xhr);
-          formData.append("eventTag", $scope.eventTag);
+          formData.append("eventTag", "tacocat");
         },
         'success': function (file, response) {
           console.log('done with sending photo');
@@ -120,15 +63,16 @@ tess.controller('DatepickerDemoCtrl', function ($scope) {
 
   tess.factory('eventFactory', ["$http", function ($http){
     var eventFactory = {};
+    // eventFactory.setEventTag = function(eventTag){
+    //   eventFactory.eventTag = eventTag;
+    // }
     eventFactory.checkForExistingEvent = function(eventTag){
-      $http.post('/event/join', {eventCode: eventTag})
-        .then(function(response){
-          console.log(response);
-        })
       console.log('handling the event checking for', eventTag);
+      // eventFactory.eventTag = eventTag;
     };
     return eventFactory;
   }]);
+
 
 
   /**
