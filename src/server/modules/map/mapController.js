85d3518ed1c2baa0ef0058a8d5cd3537eefa9e13
macklevine <mackplevine@gmail.com>
2015-09-07 21:56:38 -0700

    added mapController.js

--- src/server/modules/map/mapController.js
+++ src/server/modules/map/mapController.js
@@ -1,8 +1,27 @@
 var sendResp = require('../../config/helpers').sendResponse,
-    dB       = require('../../db/db.js');
+    db       = require('../../db/db.js');
 
+var Map      = db.Map;
+
+ //for users trying to view an event.
 module.exports = {
-  getMap: function(event){
-    
+  getMap: function(req, res){
+    Map.findOne({_parentEvent: req.body.event._id}, function(err, map){
+      if (!map){
+        console.log("error: map not found");
+      } else if (map){
+        res.send(map);
+      }
+    });
+  },
+
+  saveMap: function(req, res){
+    var conditions = {_parentEvent: req.body.event._id},
+        update     = {data: req.body.event.map.data},
+        options    = {new: true}, //guarantees that the callback returns the saved map object.
+
+    Map.findOneAndUpdate(conditions, update, options, function(err, foundMap){
+      res.send(foundMap);
+    });
   }
 };
