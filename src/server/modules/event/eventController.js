16567f895d68e0f7c7a9d5f5eb94a272c5c32ab3
macklevine <mackplevine@gmail.com>
2015-09-08 21:56:09 -0700

    added more debugging statements that can be switched on and off

--- src/server/modules/event/eventController.js
+++ src/server/modules/event/eventController.js
@@ -1,5 +1,5 @@
 var sendResp = require('../../config/helpers').sendResponse,
-    dB       = require('../../db/db'),
+    db       = require('../../db/db'),
     mapmaker = require('../../db/mapmaker'),
     cloudinary = require('../image/imageController.js');
 
@@ -11,7 +11,7 @@ module.exports = {
 
   getEvent: function (req, res){
     var eventCode = req.body.eventCode;
-    dB.Event.findOne({eventCode: eventCode}, function(err, event){
+    db.Event.findOne({eventCode: eventCode}, function(err, event){
       if (err){
         sendResp(res, err, 500);
       }
@@ -37,9 +37,11 @@ module.exports = {
         eventName = req.body.eventName,
         facebookId = req.body.facebookId;
 
-    console.log(eventCode + " is our event code");
+    // console.log(eventCode + " is our event code...");
+    // console.log(eventName + " is our event name...");
+    // console.log(facebookId + " is our facebookId...");
 
-    dB.Event.findOne({eventCode: eventCode}, function(err, event){
+    db.Event.findOne({eventCode: eventCode}, function(err, event){
       if (err){
         sendResp(res, err);
       }
