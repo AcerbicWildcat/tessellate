38ec88746bd288b037ce5adb340b65e1c0578e3e
macklevine <mackplevine@gmail.com>
2015-09-09 15:30:33 -0700

    added a dummy JSON object for Jimmy.

--- src/server/modules/user/userController.js
+++ src/server/modules/user/userController.js
@@ -4,9 +4,72 @@ module.exports = {
 
   getUserDetails: function (req, res) {
     res.json({
-      _id: "2as2agskadlawoin",
-      firstName: "Bob",
-      lastName: "Jenkins" 
+      _id: {
+        toString: function(){return "2as2agskadlawoin"}
+      },
+      facebookId: "234203kdlsjdf39fjd",
+      name: "Borp Junkums III",
+      email: "borp@junkums.io",
+      profPhoto: "some path",
+      facebookToken: {},
+      events: [
+        {
+          _creator: {
+            toString: function(){
+              return "2as2agskadlawoin";
+            }
+          },
+          contributors: [],
+          name: "Borpo's blowout",
+          eventCode: "blowout2015",
+          mainImage: {
+            imgPath: "https://secure.static.tumblr.com/54cac0794a6cb43fd4cd1fe946142290/u8ekvhx/fConapwt4/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg"
+          },
+          images: []
+        },
+        {
+          _creator: {
+            toString: function(){
+              return "sdflkajsdfj303423";
+            }
+          },
+          contributors: [],
+          eventCode: "dingus22",
+          name: "Not a Borpo party",
+          mainImage: {
+            imgPath: "https://secure.static.tumblr.com/54cac0794a6cb43fd4cd1fe946142290/u8ekvhx/fConapwt4/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg"
+          },
+          images: []
+        },
+        {
+          _creator: {
+            toString: function(){
+              return "9234jsldkjdsfjd";
+            }
+          },
+          contributors: [],
+          eventCode: "lamegradstudentparty",
+          name: "Totally Awesome Rager",
+          mainImage: {
+            imgPath: "https://secure.static.tumblr.com/54cac0794a6cb43fd4cd1fe946142290/u8ekvhx/fConapwt4/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg"
+          },
+          images: []
+        },
+        {
+          _creator: {
+            toString: function(){
+              return "2as2agskadlawoin";
+            }
+          },
+          contributors: [],
+          name: "Borpo 2: Electric Borpaloo",
+          eventCode: "borp2",
+          mainImage: {
+            imgPath: "https://secure.static.tumblr.com/54cac0794a6cb43fd4cd1fe946142290/u8ekvhx/fConapwt4/tumblr_static_party-music-hd-wallpaper-1920x1200-38501.jpg"
+          },
+          images: []
+        }
+      ]
     });
   },
   logout: function (req, res) {
