d18ef21d70e00469adc5ca7fae5d6f869eac218c
jimtom2713 <jwilliamson528@gmail.com>
2015-09-09 17:08:39 -0700

    need to wait for promised user data before loading events view

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,29 +1,16 @@
 var tess = angular.module("tessell", [
   "ngRoute"
-  // 'tessell.mosaic'
 ]);
 
 tess.config(["$routeProvider", function ($routeProvider){
     $routeProvider
-/*      .when('/events', {
-        templateUrl: '../profile.html',
-        controller: 'profileController'
-      })
-      .when('/events/create', {
-        templateUrl: '../createEvent.html',
-        controller: 'createEventController'
-      })
-      .when('/events/:id', {
-        templateUrl: '',
-        controller: ''
-      })*/
       .when('/', {
-        templateUrl: '../login.html' 
-        // controller: 'eventsProfileController',
+        templateUrl: '../login.html', 
+        controller: 'eventsProfileController'
       })
       .when('/events', {
-        templateUrl: '../events.html'
-        // controller: 'eventsProfileController'
+        templateUrl: '../events.html',
+        controller: 'eventsProfileController'
       });
       // .otherwise({
       //   //default path is back to the profile page
@@ -34,9 +21,6 @@ tess.config(["$routeProvider", function ($routeProvider){
 
 tess.run([ '$rootScope', '$location', function ($rootScope, $location){
   $rootScope.$on("$routeChangeStart", function (event, next, current){
-    // console.log("next---> ", next);
-    // console.log("event---> ", event);
-    // console.log("current---> ", current);
     //TO DO: check if user is logged in. If not send them to the login page
   });
 }]);
@@ -44,35 +28,30 @@ tess.run([ '$rootScope', '$location', function ($rootScope, $location){
 // globally available functions that make http requests to the server
 tess.factory('httpRequestFactory', [ '$http', function ($http){
   var httpRequestFactory = {};
+  httpRequestFactory.madeUserProfileRequest = false;
   httpRequestFactory.getUserProfile = function(){
+    console.log('making server request');
     return $http({
       method: 'GET',
-      url: '/user'//TBD assuming the roure is something like user/:userId?
+      url: '/user'
     }).then(function(response){
       httpRequestFactory.fullUserProfile = response;
+      httpRequestFactory.madeUserProfileRequest = true;
       return response;
     });
   };
   return httpRequestFactory;
 }]);
 
-tess.controller('mainController', [ '$scope', 'httpRequestFactory', '$location', function ($scope, httpRequestFactory, $location){
-/*  $scope.getUserProfile = function(){
-    httpRequestFactory.getUserProfile()
-      .then(function(response){
-        console.log("response from main controller --> ", response);
-      });
-  };*/
-}]);
 
-tess.controller('eventsProfileController', [ '$scope', 'eventFactory', 'httpRequestFactory', function ($scope, eventFactory, httpRequestFactory){
-  console.log($scope.eventCode);
+tess.controller('eventsProfileController', [ '$scope', 'httpRequestFactory', function ($scope, httpRequestFactory){
   $scope.getUserProfile = function(){
     httpRequestFactory.getUserProfile()
       .then(function(response){
-        console.log("response from events controller --> ", response);
+        return response;
       });
   };
+  $scope.userProfile = httpRequestFactory.fullUserProfile === undefined ? $scope.getUserProfile() : httpRequestFactory.fullUserProfile;
   $scope.joinEvent = function(){
     //TODO: code to join an exisiting event
     console.log('ready to JOIN an event');
@@ -85,12 +64,9 @@ tess.controller('eventsProfileController', [ '$scope', 'eventFactory', 'httpRequ
     //on clicking an event, take the user to that event mosaic page
     console.log("off to an exisiting event");
   };
-  $scope.userProfile = httpRequestFactory.fullUserProfile || "hello World";
 }]);
 
-tess.controller('tessellCtrl', ['$scope', "eventFactory", "$location", function ($scope, eventFactory, $location){
-  // $scope.eventTag = "";
-  // console.log('loaded Ctrl: ', $scope.mainMosaicImage);
+/*tess.controller('tessellCtrl', ['$scope', "eventFactory", "$location", function ($scope, eventFactory, $location){
   $scope.mainMosaicImage = eventFactory.mainMosaicImage;
   $scope.checkForExistingEvent = function(){
     eventFactory.checkForExistingEvent($scope.eventTag);
@@ -121,13 +97,12 @@ tess.controller('tessellCtrl', ['$scope', "eventFactory", "$location", function 
       }
     }
   };
-}]);
+}]);*/
 
-tess.factory('eventFactory', ["$http", function ($http){
+/*tess.factory('eventFactory', ["$http", function ($http){
   var eventFactory = {};
   eventFactory.mosaicRetrieved = false;
   eventFactory.getMosiacMap = function(response){
-    // console.log(response);
     if(eventFactory.mosaicRetrieved === false){
       eventFactory.mainMosaicImage = response;
       eventFactory.mosaicRetrieved = true;
@@ -138,12 +113,10 @@ tess.factory('eventFactory', ["$http", function ($http){
   eventFactory.checkForExistingEvent = function(eventTag){
     $http.post('/event/join', {eventCode: eventTag})
       .then(function(response){
-        // console.log(response);
       });
-    // console.log('handling the event checking for', eventTag);
   };
   return eventFactory;
-}]);
+}]);*/
 
 
 /**
@@ -158,18 +131,15 @@ tess.factory('eventFactory', ["$http", function ($http){
 * </div>
 */
 
-tess.directive('dropzone', function () {
+/*tess.directive('dropzone', function () {
  return function (scope, element, attrs) {
    var config, dropzone;
 
    config = scope[attrs.dropzone];
-
-   // create a Dropzone for the element with the given options
    dropzone = new Dropzone(element[0], config.options);
 
-   // bind the given event handlers
    angular.forEach(config.eventHandlers, function (handler, event) {
      dropzone.on(event, handler);
    });
  };
-});
+});*/
