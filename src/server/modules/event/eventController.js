1806e7c2e507b0dd2127322cbcfbf60599ffd542
macklevine <mackplevine@gmail.com>
2015-09-11 20:40:34 -0700

    made one small tweak

--- src/server/modules/event/eventController.js
+++ src/server/modules/event/eventController.js
@@ -81,7 +81,7 @@ module.exports = {
     // });
 
   getEvent: function (req, res){
-    var eventCode = req.body.eventCode;
+    var eventCode = req.params.eventId;
     getEventAndMap(eventCode, function(obj){
       if (typeof obj === "string"){
         sendResp(res, obj, 404);
