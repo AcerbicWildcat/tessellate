357328bac7b5ede73bed3c74f2cb5cd8cdb6fe19
Robby Hays <rhaysvm42@gmail.com>
2015-09-11 19:46:17 -0700

    Revises db createUser service to check for existing user before creating, and return user if they already exist --> closes AcerbicWildcat/tessellate#226

--- src/server/db/createUser.js
+++ src/server/db/createUser.js
@@ -4,14 +4,28 @@ var User = db.User;
 
 module.exports = function(facebookId, name, email, profPhoto, facebookToken, callback){
 
-  new User({
-    facebookId: facebookId,
-    name: name,
-    email: email,
-    profPhoto: profPhoto,
-    facebookToken: facebookToken
-  }).save(function(err, user){
-    callback(user);
+  User.findOne({'facebookId': facebookId}, function (err, user){
+
+    if (err){
+      return err;
+    }
+    if (user) {
+      callback(user);
+    }
+    else {
+      new User({
+        facebookId: facebookId,
+        name: name,
+        email: email,
+        profPhoto: profPhoto,
+        facebookToken: facebookToken
+      }).save(function(err, user){
+        if (err){
+          console.log("createUser DB Save Error:", err);
+        }
+        callback(user);
+      });
+    }
   });
 
 };
