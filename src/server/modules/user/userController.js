c32b0b9dbee4252fe67e58aef861f04f9a84e574
Robby Hays <rhaysvm42@gmail.com>
2015-09-11 20:18:08 -0700

    Fixes error when switching between mobile/desktop user facebookId lookup

--- src/server/modules/user/userController.js
+++ src/server/modules/user/userController.js
@@ -12,18 +12,28 @@ module.exports = {
   },
   createUser: function(req, res) {
     console.log(req.body, "is our body...");
-    var facebookId = req.headers.facebookid || JSON.parse(req.cookies.facebookToken).facebookId,
-        name = req.body.name,
+    var facebookId;
+    var name = req.body.name,
         profPhoto = req.body.profPhoto,
         email = req.body.email,
         facebookToken = req.body.facebookToken;
+    if (!!req.body.facebookId){
+      facebookId = req.body.facebookId;
+    } else if (!!req.cookies.facebookToken){
+      facebookId = JSON.parse(req.cookies.facebookToken).facebookId;
+    }
 
     createUser(facebookId, name, email, profPhoto, facebookToken, function(user){
         res.json(user);
     });
   },
   getUser: function(req, res) {
-    var facebookId = req.headers.facebookid || JSON.parse(req.cookies.facebookToken).facebookId;
+    var facebookId;
+    if (!!req.headers.facebookid){
+      facebookId = req.headers.facebookid;
+    } else if (!!req.cookies.facebookToken){
+      facebookId = JSON.parse(req.cookies.facebookToken).facebookId;
+    }
     console.log('facebook ID in getUSer: ', facebookId);
     getUser(facebookId, function(user){
       res.json(user);
