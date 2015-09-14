6ce478b54809393cb2b6aceeae563c4c3f20fb68
Robby Hays <rhaysvm42@gmail.com>
2015-09-13 21:32:32 -0700

    Adds error handling for Join Event & Create Event routes (Server & DB)

--- src/server/modules/event/eventController.js
+++ src/server/modules/event/eventController.js
@@ -106,11 +106,7 @@ module.exports = {
     });
   },
 
-  createEvent: function (req, res){
-
-    // for debugging
-    // console.log(req.file);
-    // console.log(req.file.path);
+  createEvent: function (req, res, next){
 
     var eventCode = req.body.eventCode,
         eventName = req.body.eventName,
@@ -122,28 +118,27 @@ module.exports = {
       facebookId = JSON.parse(req.cookies.facebookToken).facebookId;
     }
 
-    // console.log(eventCode + " is our event code...");
-    // console.log(eventName + " is our event name...");
-    // console.log(facebookId + " is our facebookId...");
-
-    db.Event.findOne({eventCode: eventCode}, function(err, event){
+    db.Event.findOne({eventCode: eventCode}, function (err, event){
       if (err){
-        sendResp(res, err);
+        next(err);
       }
       if (event){
-        sendResp(res, {event: "sorry, that event code already exists"});
+        sendResp(res, {error: "Event Code Already Taken"}, 409);
       } else {
-        cloudinary.uploader.upload(path, function(result) { 
+        cloudinary.uploader.upload(path, function (result) { 
           console.log(result.url + " is the result we got back!");
-          mapmaker.saveEventAndMap(facebookId, result.url, eventCode, eventName, function(returnObj){
-            sendResp(res, returnObj, 201);
+          mapmaker.saveEventAndMap(facebookId, result.url, eventCode, eventName, function (err, createdEvent){
+            if (err){
+              next(err);
+            }
+            sendResp(res, createdEvent, 201);
           });
         });
       }
     });
   },
 
-  joinEvent: function (req, res){
+  joinEvent: function (req, res, next){
     var eventCode = req.params.eventId;
     var facebookId;
     if (!!req.headers.facebookid){
@@ -151,14 +146,18 @@ module.exports = {
     } else if (!!req.cookies.facebookToken){
       facebookId = JSON.parse(req.cookies.facebookToken).facebookId;
     }
-    joinEvent(facebookId, eventCode, function(){
-      getEventAndMap(eventCode, function (obj){
-        if (typeof obj === "string"){
-          sendResp(res, obj, 404);
-        } else {
-          sendResp(res, obj, 200);
-        }
-      });
+    joinEvent(facebookId, eventCode, function (err, event){
+      if (err){
+        next(err);
+      } else {
+        getEventAndMap(eventCode, function (err, joinedEvent){
+          if (err){
+            next(err);
+          } else {
+            sendResp(res, joinedEvent, 200);
+          }
+        });
+      }
     });
 
   }  
