c627d6c7b829756abc9d7aeb16874e09bd4af4c2
macklevine <mackplevine@gmail.com>
2015-09-15 12:01:01 -0700

    revised error messages to send both a status code and a data object detailing the error.

--- src/server/db/joinEvent.js
+++ src/server/db/joinEvent.js
@@ -3,16 +3,16 @@ var db = require('./db.js');
 var User = db.User;
 var Event = db.Event;
 
-module.exports = function(facebookId, eventCode, done){
+module.exports = function(facebookId, eventCode, callback){
   User.findOne({facebookId: facebookId}, function (err, user){
     if (err){
-      done(err);
+      callback(err);
     }
     Event.findOne({eventCode: eventCode}, function (err, event)
       {
         console.log("the event exists", event);
       if (err){
-        done(err);
+        callback(err);
       }
       if (event){
         //check to see if the person trying to join the event is
@@ -20,29 +20,29 @@ module.exports = function(facebookId, eventCode, done){
         //verify what 'done' is doing in this context. Might want to rename it "callback."
         for (var i = 0; i < event.contributors.length; i++){
           if (event.contributors[i].toString() === user._id.toString()){
-            done(null, {error: "You've already joined this event"});
+            callback(null, {error: "You've already joined this event"});
             return;
           }
         }
         if (event._creator.toString() === user._id.toString()){
-          done(null, {error: "Sorry, this is an event you have created"});
+          callback(null, {error: "Sorry, this is an event you have created"});
           return;
         }
         user.events.push(event);
         event.contributors.push(user);
         event.save(function (err){
           if (err){
-            done(err);
+            callback(err);
           }
           user.save(function (err){
             if (err){
-              done(err);
+              callback(err);
             }
-            done(null, event);
+            callback(null, event);
           });
         });
       } else {
-        done(null, {error: "event does not exist"});
+        callback(null, {error: "event does not exist"});
       }
     });
   });
