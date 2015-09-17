ba04a5580ddbbff995435a593d2220e3988e69f8
macklevine <mackplevine@gmail.com>
2015-09-17 12:28:45 -0700

    added error handling blocks

--- src/server/db/getEventsByUser.js
+++ src/server/db/getEventsByUser.js
@@ -6,9 +6,17 @@ var User = db.User,
 
 module.exports = function(facebookId, done){
   User.findOne({facebookId: facebookId}).select("facbeookId name profPhoto facebookToken events").exec(function(err, user){
+    if(err){
+      done(err);
+    }
     User.populate(user, {path: 'events', select: "_creator name eventCode contributors mainImage"}, function(err, result){
+      if(err){
+        done(err);
+      }
       User.populate(result, {path: 'events.mainImage', model: 'Image', select: 'imgPath'}, function(err, result2){
-
+        if(err){
+          done(err);
+        }
         done(null, result2);
       });
     });
