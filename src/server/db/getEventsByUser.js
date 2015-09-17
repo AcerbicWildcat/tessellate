8a8d5306d1250179195d6941c07a3ac0d8e755f8
macklevine <mackplevine@gmail.com>
2015-09-17 12:26:01 -0700

    code cleanup

--- src/server/db/getEventsByUser.js
+++ src/server/db/getEventsByUser.js
@@ -4,8 +4,6 @@ var User = db.User,
     Event = db.Event,
     Image = db.Image;
 
-//is the facebook id what gets passed along here?
-
 module.exports = function(facebookId, done){
   User.findOne({facebookId: facebookId}).select("facbeookId name profPhoto facebookToken events").exec(function(err, user){
     User.populate(user, {path: 'events', select: "_creator name eventCode contributors mainImage"}, function(err, result){
@@ -15,27 +13,4 @@ module.exports = function(facebookId, done){
       });
     });
   });
-  //   User.populate(user, {path: 'events', select: "_creator name eventCode name contributors"}, function(err, result){
-  //       done(null, result);
-  //   });
-  // }); 
-
-}//recursive population with query is supposedly not supported.
-//     .populate('events', "_creator")
-//     .exec(function (err, docs){
-//       if (err){
-//         done(err);
-//       }
-//       var opts = {
-//         path: 'events.mainImage',
-//         model: 'Image' //could be made more lean... we only want the path, not the other stuff.
-//       };
-//       User.populate(docs, opts, function (err, result){
-//         if (err){
-//           done(err);
-//         } else {
-//           done(null, result);
-//         }
-//       });
-//     });
-// };
+}
