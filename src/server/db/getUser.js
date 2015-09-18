1f86c38526703c0403646020f7ac954aac23aff0
macklevine <mackplevine@gmail.com>
2015-09-18 12:10:15 -0700

    added documentation for all database helper functions.

--- src/server/db/getUser.js
+++ src/server/db/getUser.js
@@ -1,7 +1,12 @@
 var db = require('./db.js');
 
 var User = db.User;
-
+/**
+ * Finds a user using facebookId argument and returns the user using a callback.
+ * @param  {String}   facebookId - the Facebook ID used to look the user up by.
+ * @param  {Function} done       - the callback invoked on the found user (or the error).
+ * @return {Object}              - the user object found in the db.
+ */
 module.exports = function(facebookId, done){
   User.findOne({
     facebookId: facebookId
